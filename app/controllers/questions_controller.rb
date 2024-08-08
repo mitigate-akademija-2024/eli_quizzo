7d7c1d9e6af6a5bfeda6be47e47b235e2c6fea22
Elizxa2 <eliza.skrinda@gmail.com>
2024-08-08 14:03:12 +0000

    questions etc

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,12 @@
 class QuestionsController < ApplicationController
+    def index
+    end
+
+    def create
+    end
+
+    def new
+        @quiz = Quiz.find(params[:quiz_id])
+        @question = @quiz.questions.new
+    end
 end
