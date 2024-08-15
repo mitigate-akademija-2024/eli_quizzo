b643d607c453c37870648199fd26fee20e0505fd
Elizxa2 <eliza.skrinda@gmail.com>
2024-08-15 08:17:16 +0000

    Smukakas pogas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 # app/helpers/application_helper.rb
 module ApplicationHelper
     def btn_class
-      'inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500'
+      'inline-block rounded border border-pink-600 bg-pink-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-pink-600 focus:outline-none focus:ring active:text-pink-500'
     end
   
     def btn_class_small
-      'inline-block rounded border border-indigo-600 bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500'
+      'inline-block rounded border border-pink-600 bg-pink-600 px-4 py-2 text-xs font-medium text-white hover:bg-transparent hover:text-pink-600 focus:outline-none focus:ring active:text-pink-500'
     end
 
     def format_date(date)
